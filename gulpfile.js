2727c83620fdeb5775cd7414533b50d326e6c9ae
Emily Mu <emilymu@mit.edu>
2019-08-04 15:53:01 +0300

    Fixed scroll

--- gulpfile.js
+++ gulpfile.js
@@ -121,12 +121,16 @@ exports.default = build;
 
 //navbar funcs
 function openNav() {
+  document.getElementById("fix").style.position = "static";
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0)";
+  document.getElementById("fix").style.position = "fixed";
 }
 function closeNav() {
+  document.getElementById("fix").style.position = "static";
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
+  document.getElementById("fix").style.position = "fixed";
 }
