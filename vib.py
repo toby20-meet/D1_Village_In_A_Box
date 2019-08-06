617b5b74a5e1a0ed8cc561987c66b1401c8106ea
myname21-meet <myname21@meet.mit.edu>
2019-08-06 02:09:29 -0400

    fixed conflict

--- vib.py
+++ vib.py
@@ -6,36 +6,15 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-<<<<<<< HEAD
 	 # add_article("Your next house could save the planet","https://www.israel21c.org/topic/village-in-a-box/")
 	 # add_article("Israeli Sustainability Initiative To Pilot Village-In-A-Box With Its Own Water, Food, Energy Systems","https://nocamels.com/2019/07/israeli-sustainability-pilot-village-in-a-box-water-food-energy/")
 	 # add_article("Village-in-a-box:","https://www.mako.co.il/nexter-high-tech/Article-ab50bcaf5d10a61006.htm")
 	 # add_article("Village-in-a-box CHIVAS THE VENTURE ","https://www.foodis.co.il/village-in-a-box-chivas-the-venture/")
 	 everything = query_all_articles()
-=======
-	random_list = []
-	return render_template('index.html', random_list = random_list)
-'''
-add_article('hi', 'https://www.wikipedia.org/')
-add_article('bye', 'https://www.google.com/')
-add_article('ayy', 'https://www.youtube.com/')
-everything = query_all_articles()
-print(len(everything))
-	
->>>>>>> de44a8f9ad278ff0218c15a05df0ae0a958ecf20
 	 random_list = []
-	 values = ['Village','Community','Zero Emissions','Sustainability']
 	 for i in range(3):
-<<<<<<< HEAD
 		random_list.append(everything.pop(random.randint(0,len(everything)-1)))
-
-	 return render_template('index.html', random_list = random_list, values = values)
-=======
-		random_list.append(everything.pop(random.randint(0,len(everything))))
-	
 	 return render_template('index.html', random_list = random_list)
-	 '''
->>>>>>> de44a8f9ad278ff0218c15a05df0ae0a958ecf20
 
 @app.route('/join')
 def join():
