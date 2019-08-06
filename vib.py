3886236341a8069954a572ef7df9439d4381d6da
myname21-meet <myname21@meet.mit.edu>
2019-08-06 04:58:19 -0400

    added page

--- vib.py
+++ vib.py
@@ -5,14 +5,14 @@ from flask_mail import Mail
 from flask_mail import Message
 
 app = Flask(__name__)
-mail = Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'theybotman@gmail.com'
-app.config['MAIL_PASSWORD'] = 's0undc00kie'
+app.config['MAIL_USERNAME'] = '@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
  
 @app.route('/')
 def index():
