f38176adda1efe92093028677c63368b90c1bf94
myname21-meet <myname21@meet.mit.edu>
2019-08-07 16:25:12 +0300

    07.08

--- vib.py
+++ vib.py
@@ -39,8 +39,7 @@ def team():
 @app.route('/contact',methods = ['GET','POST']) 
 def contact():
 	 if request.method == 'POST':
-		msg = Message('village in a box contact',
-		recipients=['yoni@sustainable.co.il', 'victor@sustainable.co.il','theybotman@gmail.com'])
+		msg = Message('village in a box contact',recipients=['yoni@sustainable.co.il', 'victor@sustainable.co.il','theybotman@gmail.com'])
 		msg.html = request.form['address'] + " says:<br>"+request.form['message']
 		mail.send(msg)
 	 return render_template('contact.html')
